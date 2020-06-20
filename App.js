398f61621d2eca30c9ad0f8f8409a4e75dbbb012
eminaydin <eminaydin93@yahoo.com>
2020-06-20 22:51:41 +0200

    Add OS specific styles

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, TouchableNativeFeedback, TouchableHighlight, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, TouchableNativeFeedback, TouchableHighlight, Button, Alert, Platform, SafeAreaView, StatusBar } from 'react-native';
 
 export default function App() {
   const handlePress = () => {
@@ -7,7 +7,7 @@ export default function App() {
 
   }
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text numberOfLines={1} onPress={handlePress} >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis soluta aut pariatur sed, deserunt architecto, commodi voluptas omnis, a debitis dolore provident quos illo eligendi eaque repellendus fuga quo corporis facilis? Aliquam, natus. Ipsum facilis placeat repellendus qui nobis, laudantium rem at? Qui ducimus laborum eveniet. Enim neque tempora iusto!</Text>
       <TouchableNativeFeedback>
         <View style={{ width: 200, height: 100, backgroundColor: "gray" }}></View>
@@ -22,7 +22,7 @@ export default function App() {
       </TouchableHighlight>
       <Button title="click me" onPress={() => Alert.prompt("My title", "My message", text => console.log(text)
       )} />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -30,7 +30,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: Platform.OS === "android" ? StatusBar.currentHeight : 0
   },
 });
