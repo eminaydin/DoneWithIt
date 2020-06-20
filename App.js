965a3d1d9759a15b283aac8ce02d1d839785e8ae
eminaydin <eminaydin93@yahoo.com>
2020-06-20 22:08:25 +0200

    Add click handler on text

--- App.js
+++ App.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const handlePress = () => {
+    console.log("clicked");
+
+  }
   return (
     <View style={styles.container}>
-      <Text>Hey!</Text>
+      <Text numberOfLines={1} onPress={handlePress} >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis soluta aut pariatur sed, deserunt architecto, commodi voluptas omnis, a debitis dolore provident quos illo eligendi eaque repellendus fuga quo corporis facilis? Aliquam, natus. Ipsum facilis placeat repellendus qui nobis, laudantium rem at? Qui ducimus laborum eveniet. Enim neque tempora iusto!</Text>
     </View>
   );
 }
@@ -12,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
