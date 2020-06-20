09c4856ca3c15239e02388b79795ea4a22059737
eminaydin <eminaydin93@yahoo.com>
2020-06-20 22:10:39 +0200

    Add image

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   const handlePress = () => {
@@ -9,6 +9,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text numberOfLines={1} onPress={handlePress} >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis soluta aut pariatur sed, deserunt architecto, commodi voluptas omnis, a debitis dolore provident quos illo eligendi eaque repellendus fuga quo corporis facilis? Aliquam, natus. Ipsum facilis placeat repellendus qui nobis, laudantium rem at? Qui ducimus laborum eveniet. Enim neque tempora iusto!</Text>
+      <Image source={require(`./assets/icon.png`)} />
     </View>
   );
 }
