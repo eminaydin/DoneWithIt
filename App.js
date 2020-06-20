80d533d220c723dcd8fdd465bb4b94ccdcbadf98
eminaydin <eminaydin93@yahoo.com>
2020-06-20 22:38:16 +0200

    Use of touchables

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, TouchableNativeFeedback, TouchableHighlight } from 'react-native';
 
 export default function App() {
   const handlePress = () => {
@@ -9,7 +9,17 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text numberOfLines={1} onPress={handlePress} >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis soluta aut pariatur sed, deserunt architecto, commodi voluptas omnis, a debitis dolore provident quos illo eligendi eaque repellendus fuga quo corporis facilis? Aliquam, natus. Ipsum facilis placeat repellendus qui nobis, laudantium rem at? Qui ducimus laborum eveniet. Enim neque tempora iusto!</Text>
-      <Image source={require(`./assets/icon.png`)} />
+      <TouchableNativeFeedback>
+        <View style={{ width: 200, height: 100, backgroundColor: "gray" }}></View>
+      </TouchableNativeFeedback>
+      <TouchableOpacity>
+        <Image source={{ width: 200, height: 200, uri: "https://picsum.photos/200/300" }} />
+      </TouchableOpacity>
+      <TouchableHighlight
+        onPress={() => console.log("highlight clicked")
+        }>
+        <Image source={{ width: 200, height: 200, uri: "https://picsum.photos/200/300" }} />
+      </TouchableHighlight>
     </View>
   );
 }
@@ -17,7 +27,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
