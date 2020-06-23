eb673a1b21a307fc50fc0dec5776eafb340e6a2f
eminaydin <eminaydin93@yahoo.com>
2020-06-23 22:49:28 +0200

    Add input field

--- App.js
+++ App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, TouchableNativeFeedback, TouchableHighlight, Button, Alert, Platform, SafeAreaView, StatusBar } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, TouchableNativeFeedback, TouchableHighlight, Button, Alert, Platform, SafeAreaView, StatusBar, TextInput } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+
   const handlePress = () => {
     console.log("clicked");
 
   }
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoal(enteredText)
+  }
   return (
     <SafeAreaView style={styles.container}>
       <Text numberOfLines={1} onPress={handlePress} >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis soluta aut pariatur sed, deserunt architecto, commodi voluptas omnis, a debitis dolore provident quos illo eligendi eaque repellendus fuga quo corporis facilis? Aliquam, natus. Ipsum facilis placeat repellendus qui nobis, laudantium rem at? Qui ducimus laborum eveniet. Enim neque tempora iusto!</Text>
-      <TouchableNativeFeedback>
-        <View style={{ width: 200, height: 100, backgroundColor: "gray" }}></View>
-      </TouchableNativeFeedback>
-      <TouchableOpacity>
-        <Image source={{ width: 200, height: 200, uri: "https://picsum.photos/200/300" }} />
-      </TouchableOpacity>
-      <TouchableHighlight
-        onPress={() => console.log("highlight clicked")
-        }>
-        <Image source={{ width: 200, height: 200, uri: "https://picsum.photos/200/300" }} />
-      </TouchableHighlight>
+      <TextInput placeholder="Course Goal" onChangeText={goalInputHandler} />
+
       <Button title="click me" onPress={() => Alert.prompt("My title", "My message", text => console.log(text)
       )} />
     </SafeAreaView>
@@ -30,6 +27,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    justifyContent: "center",
     padding: Platform.OS === "android" ? StatusBar.currentHeight : 0
   },
 });
